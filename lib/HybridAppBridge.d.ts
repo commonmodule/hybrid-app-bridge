e3e041087bd302128a972287b941b6ea78964f79
yjgaia <yjdev88@gmail.com>
2024-09-19 12:58:41 +0900

    Refactor HybridAppBridge class to add support for registering and removing global functions

--- lib/HybridAppBridge.d.ts
+++ lib/HybridAppBridge.d.ts
@@ -1,5 +1,8 @@
+type GlobalFunction = (...args: any[]) => void;
 declare class HybridAppBridge {
     sendToApp(method: string, data?: any): void;
+    registerGlobalFunction(name: string, func: GlobalFunction): void;
+    removeGlobalFunction(name: string): void;
 }
 declare const _default: HybridAppBridge;
 export default _default;
--- src/HybridAppBridge.ts
+++ src/HybridAppBridge.ts
@@ -1,9 +1,28 @@
+type GlobalFunction = (...args: any[]) => void;
+
 class HybridAppBridge {
   public sendToApp(method: string, data?: any) {
     if ((window as any).messageHandler) {
       (window as any).messageHandler.postMessage(
         JSON.stringify({ method, data }),
       );
+    }
+  }
+
+  public registerGlobalFunction(name: string, func: GlobalFunction): void {
+    if (typeof (window as any)[name] !== "undefined") {
+      console.warn(
+        `A global function named '${name}' already exists. It will be overwritten.`,
+      );
+    }
+    (window as any)[name] = func;
+  }
+
+  public removeGlobalFunction(name: string): void {
+    if (typeof (window as any)[name] === "function") {
+      delete (window as any)[name];
+    } else {
+      console.warn(`No global function named '${name}' exists.`);
     }
   }
 }
